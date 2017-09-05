2cecda2dfbbc76399e2faa0f0827dbbed92d0392
Anna Delves <anna.delves@simplybusiness.co.uk>
2017-09-05 13:45:14 +0100

    Initial commit

---
+++ plane.rb
@@ -0,0 +1,15 @@
+class Plane
+
+  def initialize (location)
+    @location = location
+  end
+
+  def location
+    @location
+  end
+
+  def destination (destination)
+    @destination
+  end
+
+end
---
+++ spec/plane_spec.rb
@@ -0,0 +1,16 @@
+require './plane'
+
+  describe Plane do
+
+    it 'sets the location of the plane when initialized' do
+      annas_plane = Plane.new("stansted")
+      expect(annas_plane.location).to eq "stansted"
+    end
+
+    it 'can be given a destination' do
+      annas_plane = Plane.new("stansted")
+      annas_plane.destination("heathrow")
+      expect(annas_plane.destination).to eq "heathrow"
+    end
+
+  end
